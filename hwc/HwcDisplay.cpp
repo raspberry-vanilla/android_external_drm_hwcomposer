adcc927bd4f540a2882f24a28967b384aa278026
Konsta <konsta09@gmail.com>
2025-08-29 23:50:46 +0300

    drm_hwcomposer: Add property to disable HDR
    * It has been reported that HDR is not working properly on Raspberry Pi
      with HDMI displays that support it.
    
    Change-Id: I31f9d7bb5a3f81eeca1f98a72c26786d97edc28d

--- hwc/HwcDisplay.cpp
+++ hwc/HwcDisplay.cpp
@@ -202,6 +202,12 @@ const HwcDisplayConfig *HwcDisplay::GetNextConfig() const {
 }
 
 void HwcDisplay::SetOutputType(OutputType hdr_output_type) {
+  if (Properties::DisableHdr()) {
+    hdr_metadata_.reset();
+    min_bpc_ = 6;
+    colorspace_ = Colorspace::kDefault;
+    return;
+  }
   switch (hdr_output_type) {
     case OutputType::kHdr10: {
       SetHdrOutputMetadata(ui::Hdr::HDR10);
--- utils/properties.cpp
+++ utils/properties.cpp
@@ -150,4 +150,8 @@ auto Properties::GetForceMode() -> std::string {
   return {force_mode};
 }
 
+auto Properties::DisableHdr() -> bool {
+  return (property_get_bool("vendor.hwc.drm.disable_hdr", 0) != 0);
+}
+
 }  // namespace android::drm_hwcomposer
